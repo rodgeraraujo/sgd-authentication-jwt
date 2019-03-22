2f9a3d6a08b2c8da852bc6172acbe78056641130
rodgeraraujo <rogerio.araujo@mail.com>
2019-03-22 09:39:37 -0300

    enviando...

---
+++ src/main/java/br/edu/ifpb/sgd/jwt/JwtmeApplication.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.sgd.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Rodger
+ */
+
+@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
+public class JwtmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtmeApplication.class, args);
+    }
+
+    @RequestMapping("/home")
+    public String hello() {
+        return "Hello buddy!";
+    }
+}
